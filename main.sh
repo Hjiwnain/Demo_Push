b36add3aa5ad27548ec84964a889c8c0447bd4c8
Hemang Jiwnani <Hemang.jiwnani@iail.in>
2022-08-17 10:10:10 +0530

    commit20231020

---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git init
+touch junk
+
+declare -i x
+echo "Enter Beginning date [yyyymmdd]"
+read x
+
+declare -i y
+echo "Enter End date [yyyymmdd]"
+read y
+
+DATE=$x
+
+while [ $DATE -le $y ]; do
+    echo 'a' >>junk
+
+    git add .
+
+    msg="commit$DATE"
+    # echo ("$DATE + 1 day" +%Y%m%d)
+    # Use double quotes around the commit message and date
+    git commit -m "$msg" --date="$ 17/08/22 10:10:10"
+
+    # Calculate the next date using the 'date' command
+    DATE=$(date -d "$DATE + 1 day" +%Y%m%d)
+done
